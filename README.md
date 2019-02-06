d158d7b6b018cd2bd1d801163d4f0054dfe2c509
Helio <helio.ribeiro@urbemobile.com.br>
2019-02-06 01:39:49 -0200

    ajustes

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 Example of web scraping public data using BeatifullSoup and Python.
 The data used in this example was obtained from a public repository available on https://brasil.io/
-This example shows how to obtain the Payroll of Brazilian Courts's magistrates from each state and export it to a CSV file for visualization.
+This example shows how to obtain the Payroll of Brazilian Courts's magistrates from each state and insert to mongodb.
   
 Exemplo de raspagem de dados públicos utilizando BeatifullSoup em Python.
 Os dados utilizados neste exemplos foram obtidos do repositório de dados públicos disponibilizados em https://brasil.io/
-O exemplo obtém os dados de Pagamentos efetuados aos magistrados dos tribunais de cada estado e exportados para um arquivo CSV para ser visualizado em formato de planilha.
+O exemplo obtém os dados de Pagamentos efetuados aos magistrados dos tribunais de cada estado e insere na base de dados do mongodb.
+
+Python versão 3.7.0  
+
+Instalar as dependências: `pip install -r requirements.txt`  
+  
+A configuração do database é feita em [DATABASE] do arquivo config.ini  
+  
+Executar p script  
+`python main.py`  
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+import pymongo
+import configparser
+
+
+def connect_database():
+    '''
+        connect to mongodb
+        @return db object
+    '''
+    config = configparser.ConfigParser()        
+    config.read('config.ini')
+    db_conf = config["DATABASE"]
+
+    conn_str = 'mongodb://{0}:{1}@{2}:{3}/{4}'.format(db_conf["user"],
+                                                          db_conf["password"],
+                                                          db_conf["host"],
+                                                          db_conf["port"],
+                                                          db_conf["dbname"])
+    conn = pymongo.MongoClient(conn_str)
+    return conn[db_conf["dbname"]]    
 
 def run():       
     
@@ -14,13 +33,12 @@ def run():
     li     =  ul.findAll('li')
     li     =  li[0]
     page_text = li.text # pega o conteudo do elemtno li
-    num_pages =  page_text.strip().split(" ")[5] #  quebra o texto e paga o num de paginas
+    num_pages =  int(page_text.strip().split(" ")[5].replace(".","")) #  quebra o texto e paga o num de paginas
     
     header = []
-    t_info = []
-    
+    t_info = []  
     for i in range(int(num_pages)):
-        
+        if i>1: break
         # pega os dados da tabela por pagina i
         html_page = requests.get(LINK + FILTER.format(i))
         soup   = BeautifulSoup(html_page.text, 'html.parser') 
@@ -31,21 +49,27 @@ def run():
             if '<th>' in str(line) and len(header) == 0:
                 ths = line.findAll('th')        
                 for data in ths:                    
-                    header.append(data.text.encode("utf-8"))     
+                    header.append(data.text)     
                 continue
                 # print result            
-            tds = line.findAll('td') 
-                       
+            tds = line.findAll('td')                        
             data_dict = {} 
             for pos in range(len(header)):
-                data_dict[header[pos]] = tds[pos].text.encode("utf-8") if len(tds)> pos else ''   
+                data_dict[header[pos]] = tds[pos].text if len(tds)> pos else ''   
             t_info.append(data_dict)
           
-    # GRAVA OS DADOS EM ARQUIVO CSV
-    with open('salarios.csv', 'w') as csvfile:      
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
+    # GRAVA OS DADOS NO BANCO             
+    try:
+        db = connect_database()
         for data in t_info:
-            writer.writerow(data)
+            db["info_salarios"].update_one(data,{'$set': data },upsert=True)
+    except Exception as e:
+        print('error inserting data: {}'.format(str(e))) 
       
-run() 
+if __name__ == '__main__':
+    try:
+        print('started script')
+        run()
+        print('finished script')
+    except Exception as e:
+        print('error processing -- {}'.format(str(e)))
